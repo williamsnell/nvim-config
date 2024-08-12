f896a45ad18a664fe8da4f5b74e456da5591310e
wsnell <aussiesnells@gmail.com>
2024-08-12 14:45:50 +1200

    add toggleterm

---
+++ after/plugin/toggleterm.lua
@@ -0,0 +1,5 @@
+require("toggleterm").setup{
+  -- size can be a number or function which is passed the current terminal
+  open_mapping = [[<c-\>]], -- or { [[<c-\>]], [[<c-¥>]] } if you also use a Japanese keyboard.
+}
+
